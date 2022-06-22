65c099483080308c7ddbeb9f91d5a4c24826d2ec
user03 <user03@DESKTOP-2JJDG7F>
2022-06-22 10:17:35 +0900

    security 분리

--- demo/src/main/java/com/example/demo/jwt/PermissionInterceptor.java
+++ demo/src/main/java/com/example/demo/jwt/PermissionInterceptor.java
@@ -31,7 +31,7 @@ public class PermissionInterceptor implements HandlerInterceptor {
 		String token = request.getHeader("token"); // client에서 요청할 때 header에 넣어둔 "jwt-auth-token"이라는 키 값을 확인
 
 		HttpSession session = request.getSession();
-		if (url.contains("singup") || url.contains("login") ) { // 토큰이 없고
+		if (url.contains("signup") || url.contains("login") ) { // 토큰이 없고
 			if (session.getAttribute("userinfo") == null) { //login x null
 				logger.info("PermissionInterceptor - {}", "login X " + url);
 				session.invalidate(); // 정보삭제
