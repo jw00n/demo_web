d117724a9ad7913e110f6b2d7342983a871aa8d7
user03 <user03@DESKTOP-2JJDG7F>
2022-06-29 17:14:49 +0900

    end

--- demo/scripts/profile.sh
+++ demo/scripts/profile.sh
@@ -3,7 +3,8 @@
 function find_idle_profile(){
     
     RESPONSE_CODE=$(curl -L -s -o /dev/null -w "%{http_code}" https://momodemo.ddns.net/profile)
-    
+    echo "$RESPONSE_CODE"
+    echo "$CURRENT_PROFILE"
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=prof2
@@ -11,7 +12,7 @@ function find_idle_profile(){
         CURRENT_PROFILE=$(curl -L -s https://momodemo.ddns.net/profile)
     fi
 
-    if [ ${CURRENT_PROFILE} == prof1]
+    if [ ${CURRENT_PROFILE} == prof1 ]
     then
       IDLE_PROFILE=prof2
     else
