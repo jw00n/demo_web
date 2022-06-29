61231a57f0d670b0c7694e22ef9b687454cb1f55
user03 <user03@DESKTOP-2JJDG7F>
2022-06-29 15:42:10 +0900

    yes you can!!

--- demo/scripts/start.sh
+++ demo/scripts/start.sh
@@ -18,14 +18,14 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar \
+sudo nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ubuntu/web/app/application-db.properties,/home/ubuntu/web/app/application-secret.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
