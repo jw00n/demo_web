f49c43b7c448cdd56e845725e509ed16a5a5afc8
user03 <user03@DESKTOP-2JJDG7F>
2022-06-29 18:41:25 +0900

    rer

--- demo/scripts/health.sh
+++ demo/scripts/health.sh
@@ -9,12 +9,12 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -L -s https://momodemo.ddns.net/profile "
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-  RESPONSE=$(curl -L -s https://momodemo.ddns.net/profile)
+  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'prof' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
